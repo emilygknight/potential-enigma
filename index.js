08cc75338bb14aaa88e0d45e12c67871dfc9073d
emilygknight <emilyknight819@gmail.com>
2023-10-09 17:19:48 -0500

    got questions to work in cammand line and added fs

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require("fs") // this si alibrary that deals with creating files or editing files etc..
+
+
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Whats the title of your application?'
+    }, {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?"
+    }, {
+        type: "input",
+        name: "installation",
+        message: "What are the instructions to install your application?"
+    }, {
+        type: "input",
+        name: "usage",
+        message: "How do you use this application?"
+    }, {
+        type: "input",
+        name: "contributions",
+        message: "What contributions did you use?"
+    }, {
+        type: "input",
+        name: "tests",
+        message: "How do you test this application?"
+    }, {
+        type: "list",
+        name: "license",
+        message: "what license did you use for this project ?",
+        choices: ["MIT", "APACHE", "NONE"]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+
+    fs.writeFile("READE.md", data)
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function (answers) {
+            console.log("THE ANSWERS!!!! ", answers);
+            console.log("This is title ", answers.title)
+
+            var template = `## ${answers.title}`
+            writeToFile(template);
+
+        })
+
+
+}
 
 // Function call to initialize app
 init();
+
